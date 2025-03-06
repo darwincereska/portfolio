fe9b1a1484494dda6e997987d475f909987eb270
Darwin Cereska <accfordarwin@gmail.com>
2025-03-05 21:52:38 -0500

    updated constants

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Darwin Cereska';
-export const SITE_DESCRIPTION = '16 year old developer';
+export const SITE_DESCRIPTION = 'Darwin Cereska, a 16 year old developer';
