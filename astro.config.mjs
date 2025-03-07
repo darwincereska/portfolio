ac6c91554702555fb27f887612ec9e9bd32ed4ee
Darwin Cereska <accfordarwin@gmail.com>
2025-03-06 21:13:28 -0500

    added some new fancy plugins, and increased server performance 3x

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,23 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import playformCompress from '@playform/compress';
+
+import purgecss from 'astro-purgecss';
+import swup from '@swup/astro';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://darwincereska.dev',
-	integrations: [mdx(), sitemap()],
+    site: 'https://darwincereska.dev',
+    integrations: [mdx(), sitemap(), purgecss(), playformCompress({
+      CSS: true,
+      HTML: {
+        "html-minifier-terser": {
+        removeAttributeQuotes: false,
+        },
+      },
+      Image: true,
+      JavaScript: true,
+      SVG: true,
+      }), swup({smoothScrolling: true, cache: true, preload: true, loadOnIdle: true, containers: ["main"], })],
 });
